097aec061f549605294a60ae10c2888fc88ad4c0
repli2dev <repli2dev@d47ec115-ae4b-0410-a58f-bf2cf29c2aa3>
2009-07-19 10:03:40 +0000

    * Vzhled administrace
    git-svn-id: http://preader.googlecode.com/svn/trunk@259 d47ec115-ae4b-0410-a58f-bf2cf29c2aa3

--- app/presenters/BackendPresenter.php
+++ app/presenters/BackendPresenter.php
@@ -15,9 +15,19 @@ abstract class BackendPresenter extends BasePresenter
 		parent::beforeRender();
 		// Basic template
 		$this->template->title = Locales::get()->get("administration");
+        $this->template->currentUser = Locales::get('users')->get("current_user");
+        $this->template->back= Locales::get()->get("back");
+        $this->template->settings= Locales::get()->get("settings");
+        $this->template->modules= Locales::get()->get("modules");
+        $this->template->logout= Locales::get('users')->get("logout");
+        $domain = Site::getInstance()->getDomain();
+        $this->template->domain = $domain[Domain::DATA_URI];
+        $this->template->date = date("Y-m-d H:i:s");
+        $this->template->user = Environment::getUser()->getIdentity();
+        $this->template->link_logout  = NULL;
 
 		// Menu
-		foreach(Modules::getInstance()->installedModules() AS $moduleName) {
+		foreach($modules  AS $moduleName) {
 			$links = Modules::getInstance()->get($moduleName)->getSection(ModuleSection::BACKEND)->getLinks();
 			foreach ($links AS $name => $descriptor) {
 				$name = Locales::get($moduleName)->get($name);
