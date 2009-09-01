de7074f825000cfeae50153efe2f55dd6040d447
jan.papousek <jan.papousek@d47ec115-ae4b-0410-a58f-bf2cf29c2aa3>
2009-09-01 08:30:10 +0000

    [Books] Podobnost
    git-svn-id: http://preader.googlecode.com/svn/trunk@370 d47ec115-ae4b-0410-a58f-bf2cf29c2aa3

--- document_root/book_similarity.php
+++ document_root/book_similarity.php
@@ -20,14 +20,14 @@ dibi::query("DROP TABLE IF EXISTS [book_similarity]");
 echo dibi::$elapsedTime;
 
 echo "\n Create table with similarity... ";
-dibi::query("CREATE TABLE [book_similarity] SELECT
+dibi::query("CREATE TABLE [book_similarity] (INDEX([id_book_from]), INDEX([id_book_to])) SELECT
                 `from`.`id_book` AS `id_book_from`,
                 `to`.`id_book`  AS `id_book_to`,
-                2 * COUNT(*) /(SELECT COUNT(`id_tag`) FROM `tagged` WHERE `id_book` = `from`.`id_book` OR `id_book` = `to`.`id_book`) AS `value`
+                2 * COUNT([from].[id_tagged]) / (SELECT COUNT([id_tagged]) FROM [tagged] WHERE [id_book] = [from].[id_book] OR [id_book] = [to].[id_book]) AS [value]
         FROM `tagged` AS `from`
         INNER JOIN `tagged` AS `to` ON `to`.`id_tag` = `from`.`id_tag`
         WHERE `from`.`id_book` != `to`.`id_book`
-        GROUP BY `from`.`id_book`, `to`.`id_book`;
+        GROUP BY `from`.`id_book`, `to`.`id_book`
 ");
 echo dibi::$elapsedTime;
  
