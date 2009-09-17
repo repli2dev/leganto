536ebac3938067b0048f57ec2bc071de556f6f4d
jan.papousek <jan.papousek@d47ec115-ae4b-0410-a58f-bf2cf29c2aa3>
2009-09-17 18:43:55 +0000

    [Similarity] zacatek zobecnovani podobnosti
    git-svn-id: http://preader.googlecode.com/svn/trunk@379 d47ec115-ae4b-0410-a58f-bf2cf29c2aa3

--- document_root/book_similarity.php
+++ document_root/book_similarity.php
@@ -1,26 +1,5 @@
 <?php
 require_once(dirname(__FILE__) . "/constants.php");
-
-define("LIMIT_OF_TAGS", 5);
-define("LIMIT_OF_RELATIVE_VALUE", 0.25);
-define("LIMIT_OF_ABSOLUTE_VALUE", 3);
-
-echo "\nLIMIT OF TAGS: " . LIMIT_OF_TAGS;
-echo "\nLIMIT OF ABSOLUTE VALUE: " . LIMIT_OF_ABSOLUTE_VALUE;
-echo "\nLIMIT OF RELATIVE VALUE: " . LIMIT_OF_RELATIVE_VALUE;
-
-echo "\n";
-
-function key_compare_func($key1, $key2)
-{
-    if ($key1 == $key2)
-        return 0;
-    else if ($key1 > $key2)
-        return 1;
-    else
-        return -1;
-}
-
 require_once LIBS_DIR . '/Nette/loader.php';
 
 // Loader
@@ -31,71 +10,16 @@ $loader->register();
 
 Debug::enable(Debug::DEVELOPMENT);
 
+Debug::timer();
+
 Environment::loadConfig(APP_DIR . '/config.ini');
 
 dibi::connect(Environment::getConfig("database"));
 
-$last = dibi::query("SELECT MAX([updated]) AS [last] FROM [book_similarity]")->fetch();
+$similarity = new DatabaseSimilarity("tagged", "book", "tag");
+$similarity->checkout();
 
-echo "\nLAST UPDATED: ".(empty($last["last"]) ? "NEVER" : $last["last"]);
-
-$books = dibi::query("SELECT * FROM [tagged] GROUP BY [id_book] HAVING COUNT([id_tag]) >= %i", LIMIT_OF_TAGS)->fetchPairs("id_book","id_book");
-$source = dibi::dataSource("SELECT * FROM [tagged] GROUP BY [id_book] HAVING COUNT([id_tag]) >= %i ORDER BY [updated] DESC", LIMIT_OF_TAGS);
-if (!empty($last["last"])) {
-	$source->where("[updated] > %t", $last["last"]);
-}
-$updatedBooks = $source->fetchPairs("id_book","id_book");
-$tags = dibi::query("SELECT [id_book], [id_tag] FROM [tagged] WHERE [id_book] IN %l", $books)->fetchAssoc("id_book,id_tag");
-
-echo "\nNUMBER OF SELECTED BOOKS: ".count($books);
-echo "\nNUMBER OF BOOKS TO UPDATE: ".count($updatedBooks);
-echo "\n\n";
-
-dibi::query("DELETE FROM [book_similarity] WHERE [id_book_to] IN %l", $updatedBooks, " OR [id_book_from] IN %l", $updatedBooks);
-
-$together = 0;
-$insertedBooks = 0;
-$maximum = 0;
-$minimum = 0;
-
-foreach ($books AS $from) {
-	$fromTags = $tags[$from];
-	$inserted = 0;
-	foreach($books AS $to) {
-		if ($from == $to) {
-			continue;
-		}
-		if (!in_array($from, $updatedBooks) && !in_array($to, $updatedBooks)) {
-			continue;
-		}
-		$toTags = $tags[$to];
-		$intersect = array_intersect_ukey($fromTags, $toTags,'key_compare_func');
-		if ($intersect < LIMIT_OF_ABSOLUTE_VALUE) {
-			continue;
-		}
-		$value = count($intersect)/count($fromTags);
-		if ($value < LIMIT_OF_RELATIVE_VALUE) {
-			continue;
-		}
-		dibi::insert("book_similarity", array("id_book_from" => $from, "id_book_to" => $to, "value" => $value))->execute();
-		$inserted++;
-	}
-	if ($inserted > $maximum) {
-		$maximum = $inserted;
-	}
-	if ($inserted < $minimum) {
-		$minimum = $inserted;
-	}
-	if ($inserted > 0) {
-		$insertedBooks++;
-	}
-	$together += $inserted;
-}
-
-echo "\nNUMBER OF INSERTED ROWS: ".$together;
-echo "\nNUMBER OF INSERTED BOOKS: ".$insertedBooks;
-echo "\nMAXIMUM OF SIMILAR BOOKS: ". $maximum;
-echo "\nMINUMUM OF SIMILAR BOOKS: ". $minimum;
+echo "\nELAPSED TIME: ".Debug::timer()."\n";
 
 die("\nDONE\n");
 ?>
--- document_root/user_similarity.php
+++ document_root/user_similarity.php
@@ -9,37 +9,18 @@ $loader->addDirectory(APP_DIR);
 $loader->addDirectory(LIBS_DIR);
 $loader->register();
 
+Debug::timer();
+
 Debug::enable(Debug::DEVELOPMENT);
 
 Environment::loadConfig(APP_DIR . '/config.ini');
 
 dibi::connect(Environment::getConfig("database"));
 
-echo "\n Dropping table with similarity... ";
-dibi::query("DROP TABLE IF EXISTS [user_similarity]");
-echo dibi::$elapsedTime;
+$similarity = new DatabaseSimilarity("opinion", "user", "book", "rating", 5);
+$similarity->checkout();
 
-echo "\n Creating table user_similarity... ";
-dibi::query("CREATE TABLE [user_similarity]
-			(INDEX([id_user_to]), INDEX([id_user_from]))
-			SELECT
-                `from`.`id_user` AS `id_user_from`,
-                SUM(
-                        CASE ABS(`from`.`rating` - `to`.`rating`)
-                                WHEN 0 THEN 16
-                                WHEN 1 THEN 9
-                                WHEN 2 THEN 4
-                                WHEN 3 THEN 2
-                                WHEN 4 THEN 0
-                        END
-                ) / (SELECT COUNT(*)*16 FROM [opinion] WHERE [id_user] = [id_user_from]) AS `value`,
-                `to`.`id_user`  AS `id_user_to`
-        FROM `opinion` AS `from`
-        INNER JOIN opinion AS `to` ON `to`.id_book = `from`.id_book
-        WHERE `from`.`id_user` != `to`.`id_user`
-        GROUP BY `from`.id_user, `to`.id_user
-");
-echo dibi::$elapsedTime;
- 
-die("\nDONE\n");
+echo "\nELAPSED TIME: ".Debug::timer()."\n";
+
+echo "\nDONE\n";
 ?>
