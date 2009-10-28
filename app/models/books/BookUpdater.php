c6df66a143cf93bfa647ae3d823462621cb19757
jan.papousek <jan.papousek@d47ec115-ae4b-0410-a58f-bf2cf29c2aa3>
2009-10-28 13:11:05 +0000

    Updatovani knihy - autori a tagy
    git-svn-id: http://preader.googlecode.com/svn/trunk@395 d47ec115-ae4b-0410-a58f-bf2cf29c2aa3

--- app/models/books/BookUpdater.php
+++ app/models/books/BookUpdater.php
@@ -27,39 +27,78 @@ class BookUpdater extends Worker implements IUpdater
 		SimpleTableModel::createTableModel("book_title")->update($entity->getId(), $input);
 	}
 
-	public function setWrittenBy(BookEntity $book, $authors) {
+	/**
+	 * It sets author(s) as an author of the book
+	 *
+	 * @param BookEntity $book
+	 * @param array|AuthorEntity $writtenBy
+	 * @throws InvalidArgumentException if the $writtenBy is not an AuthorEntity (array)
+	 */
+	public function setWrittenBy(BookEntity $book, $writtenBy) {
 		if (!$book->isReadyToUpdate()) {
 			throw new InvalidArgumentException("The entity is not ready to be updated.");
 		}
-		// delete all old relations between the book and authors
-		dibi::delete("written_by")->where("[id_book] = %i",$book->bookNode)->execute();
-		// add new relations
-		foreach($authors as $author){
-			dibi::insert("written_by",
+		// I want to add a set of authors
+		if (is_array($writtenBy)) {
+			dibi::begin();
+			// delete all old relations between the book and authors
+			dibi::delete("written_by")->where("[id_book] = %i",$book->bookNode)->execute();
+			// add new relations
+			foreach($writtenBy as $author){
+				dibi::insert("written_by",
+					array(
+						"id_book" => $book->bookNode,
+						"id_author" => $author->getId()
+					)
+				)->execute();
+			}
+			dibi::commit();
+		}
+		// I want to add only one author
+		else if($writtenBy instanceof AuthorEntity) {
+			SimpleTableModel::createTableModel("written_by")->insert(array(
 				array(
 					"id_book" => $book->bookNode,
 					"id_author" => $author->getId()
 				)
-			)->execute();
+			));
 		}
+		else {
+			throw new InvalidArgumentException("The argument [writtenBy] has to be array or AuthorEntity.");
+		}
+	
 	}
 
 	/**
 	 * It tags a book
 	 *
-	 * @param int $book Book ID
-	 * @param int $tag Tag ID
+	 * @param BookEntity $book 
+	 * @param array|TagEntity $tag
 	 */
-	public function setTagged(BookEntity $book, $tag) {
-		$rows = SimpleTableModel::createTableModel("tagged")
-			->findAll()
-			->where("[id_book] = %i", $book)
-			->where("[id_tag] = %i", $tag);
-		if ($rows->count() == 0) {
+	public function setTagged(BookEntity $book, $tagged) {
+		if (is_array($tagged)) {
+			dibi::begin();
+			// Delete old relations between tags and books
+			dibi::delete("tagged")
+				->where("[id_book] = %i", $book->bookNode)
+				->execute();
+			// Add new relations
+			foreach($tagged AS $tag) {
+				dibi::insert("tagged", array(
+					"id_tag"	=> $tag->getId(),
+					"id_book"	=> $book->getId()
+				))->execute();
+			}
+			dibi::commit();
+		}
+		else if ($tagged instanceof TagEntity) {
 			SimpleTableModel::createTableModel("tagged")->insert(array(
 				"id_book" => $book,
 				"id_tag" => $tag
 			));
+		}
+		else {
+			throw new InvalidArgumentException("The argument [tagged] has to be array or TagEntity.");
 		}
 	}
 
