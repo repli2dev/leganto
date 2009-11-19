a0f39ae75c928c7de6e6245770b3345c5afbd190
jan.papousek <jan.papousek@d47ec115-ae4b-0410-a58f-bf2cf29c2aa3>
2009-11-19 16:06:51 +0000

    git-svn-id: http://preader.googlecode.com/svn/trunk@471 d47ec115-ae4b-0410-a58f-bf2cf29c2aa3

---
+++ leganto/document_root/importEditions.php
@@ -0,0 +1,43 @@
+<?php
+require_once(dirname(__FILE__) . "/constants.php");
+require_once LIBS_DIR . '/Nette/loader.php';
+
+// Loader
+$loader = new RobotLoader();
+$loader->addDirectory(APP_DIR);
+$loader->addDirectory(LIBS_DIR);
+$loader->register();
+
+Debug::enable(Debug::DEVELOPMENT);
+
+Environment::loadConfig(APP_DIR . '/config.ini');
+
+dibi::connect(Environment::getConfig("database"));
+
+// Ziskam knihu
+$books           = Leganto::books()->fetchAndCreateAll(Leganto::books()->getSelector()->findAll()->applyLimit(20));
+// Vytvorim vyhledavac obrazku
+$imageFinder    = new EditionImageFinder();
+// Vytvorim uloziste obrazku
+$storage        = new EditionImageStorage();
+// Vytvorim Google Books finder urcite jazykove verze
+$googleFinder   = new GoogleBooksBookFinder("cs");
+
+foreach ($books AS $book) {
+	// Ziskam data od Googlu
+	$info           = $googleFinder->get($book);
+	if (empty($info)) {
+		continue;
+	}
+	// Na zaklade dat od Googlu vytvorim k dane knize edice
+	$editions       = Leganto::editions()->getInserter()->insertByGoogleBooksInfo($book, $info);
+	// Pro kazdou edici
+	foreach($editions AS $edition) {
+		// Ziskam obrazky (teoreticky je jich vice, i kdyz me to ted nezajima)
+		$images = $imageFinder->get($edition);
+		// Pokud nejsou obrazky prazdne, ulozim prvni z nich
+		if (!empty($images)) {
+			$storage->store($edition, new File(ExtraArray::firstValue($images)));
+		}
+	}
+}
