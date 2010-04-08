17df03dbb4c5119e55ef863ef8ec9939295692ff
Jan Papoušek <papi@papi-laptop.(none)>
2010-04-08 18:35:03 +0200

    bug fixed

--- leganto/app/WebModule/presenters/BookPresenter.php
+++ leganto/app/WebModule/presenters/BookPresenter.php
@@ -65,7 +65,7 @@ class Web_BookPresenter extends Web_BasePresenter
 
     protected function createComponentSubmenu($name) {
 	$submenu = new SubmenuComponent($this, $name);
-	$submenu->addLink("default", System::translate("General info"));
+	$submenu->addLink("default", System::translate("General info"), array("book" => $this->getTemplate()->book->getId()));
 	$submenu->addLink("opinions", System::translate("Opinions"), array("book" => $this->getTemplate()->book->getId()));
 	$submenu->addLink("similar", System::translate("Similar books"), array("book" => $this->getTemplate()->book->getId()));
 	return $submenu;
