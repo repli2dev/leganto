250263fc6c21feee8e1fc2a65a78a4e6c695b8ad
Jan Papoušek <jan.papousek@gmail.com>
2010-08-03 15:34:05 +0200

    Statistiky odstraneny z hlavniho menu

--- leganto/app/WebModule/presenters/StatisticsPresenter.php
+++ leganto/app/WebModule/presenters/StatisticsPresenter.php
@@ -6,6 +6,7 @@ class Web_StatisticsPresenter extends Web_BasePresenter
     public function renderDefault() {
 	$this->getTemplate()->opinionsByRating	= StatisticsGraphs::getNumberOfOpinionsByRating()->getLink();
 	$this->getTemplate()->opinionsLastYear	= StatisticsGraphs::getNumberOfInsertedOpinionsLastYear()->getLink();
+	$this->setPageTitle(System::translate("Statistics"));
     }
 
 }
