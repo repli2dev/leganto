727b332748e2a6c958243ec6d05784adeaf56c0f
Jan Drabek <me@jandrabek.cz>
2010-08-03 16:10:45 +0200

    * Vyreseni duplicitnich ISBN, pridani pravidel

--- leganto/app/WebModule/components/Edition/EditionComponent.php
+++ leganto/app/WebModule/components/Edition/EditionComponent.php
@@ -51,18 +51,32 @@ class EditionComponent extends BaseComponent {
 		return $form;
 	}
 
-	protected function prepareForm() {
+	protected function prepareForm($owner = "add") {
 		$form = new BaseForm;
-		$form->addText("isbn10", "ISBN 10")
-			->addRule(Form::MAX_LENGTH,"Maximal length can be 10 characters.",10)
-			->addRule(Form::MIN_LENGTH,"Minimal length can be 10 characters.",10)
+		$form->addText("isbn10", "ISBN 10",NULL,10)
 			->setOption("description",System::translate("Nine characters long ISBN."));
-		$form->addText("isbn13", "ISBN 13")
-			->addRule(Form::MAX_LENGTH,"Maximal length can be 13 characters.",13)
-			->addRule(Form::MIN_LENGTH,"Minimal length can be 13 characters.",13)
+		$form->addText("isbn13", "ISBN 13",NULL,13)
 			->setOption("description",System::translate("Thirdteen characters long ISBN."));
 		$form->addText("pages","Number of pages");
 		$form->addText("published","Year of publishing");
+		$form->addFile("image", "Book cover")
+			->addCondition(Form::FILLED)
+			->addRule(Form::MIME_TYPE, "File must be an image.", 'image/*')
+			->addRule(Form::MAX_FILE_SIZE,"Image has to be smaller than 100 KB.",1024*100);
+		// Add cross rules
+		$form["isbn10"]->addCondition(Form::FILLED)
+			->addRule(Form::MAX_LENGTH,"Maximal length can be 10 characters.",10)
+			->addRule(Form::MIN_LENGTH,"Minimal length can be 10 characters.",10);
+		$form["isbn13"]->addCondition(Form::FILLED)
+			->addRule(Form::MAX_LENGTH,"Maximal length can be 13 characters.",13)
+			->addRule(Form::MIN_LENGTH,"Minimal length can be 13 characters.",13);
+		$form["isbn10"]->addCondition(~Form::FILLED)
+			->addConditionOn($form["isbn13"],~Form::FILLED)
+			->addRule(Form::FILLED,"Please fill at least one of ISBNs.");
+
+		if($owner == "edit") {
+			$form["image"]->setOption("description",System::translate("(only if change)"));
+		}
 		return $form;
 	}
 
@@ -79,7 +93,12 @@ class EditionComponent extends BaseComponent {
 		$entity->published = $values["published"];
 		$entity->inserted = new DateTime();
 		$entity->persist();
-		$this->getPresenter()->flashMessage(System::translate("Thank you. Your edition was successfully added and you are looking on it now."));
+		$tmpFile = $values["image"]->getTemporaryFile();
+		if (!empty($tmpFile)) {
+			$storage = new EditionImageStorage();
+			$storage->store($entity, new File($tmpFile));
+		}
+		$this->getPresenter()->flashMessage(System::translate("Thank you. Your edition has been successfully added and you are looking on it now."));
 		$this->getPresenter()->redirect("Book:default",$book,$entity->getId());
 	}
 
@@ -89,14 +108,25 @@ class EditionComponent extends BaseComponent {
 		
 		$values = $form->getValues();
 
-		$entity = Leganto::editions()->getSelector()->find($edition);
-		$entity->isbn10 = $values["isbn10"];
-		$entity->isbn13 = $values["isbn13"];
-		$entity->pages = $values["pages"];
-		$entity->published = $values["published"];
-		$entity->persist();
-		$this->getPresenter()->flashMessage(System::translate("Thank you. Edition was successfully updated and you are looking on it now."));
-		$this->getPresenter()->redirect("Book:default",$book,$entity->getId());
+		try {
+			$entity = Leganto::editions()->getSelector()->find($edition);
+			$entity->isbn10 = $values["isbn10"];
+			$entity->isbn13 = $values["isbn13"];
+			$entity->pages = $values["pages"];
+			$entity->published = $values["published"];
+			$entity->persist();
+			$tmpFile = $values["image"]->getTemporaryFile();
+			if (!empty($tmpFile)) {
+				$storage = new EditionImageStorage();
+				$storage->store($entity, new File($tmpFile));
+			}
+			$this->getPresenter()->flashMessage(System::translate("Thank you. Edition has been successfully updated and you are looking on it now."));
+			$this->getPresenter()->redirect("Book:default",$book,$entity->getId());
+		}
+		catch (DuplicityException $e) {
+			$this->getPresenter()->flashMessage(System::translate("Sorry. However edition with this ISBN already exists. Here is form with original values."));
+			$this->getPresenter()->redirect("this");
+		}
 		
 	}
 
