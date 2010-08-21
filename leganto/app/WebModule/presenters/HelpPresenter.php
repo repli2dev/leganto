182b1d98c71f77435e381943ba258a37b3ecfe39
Jan Drabek <me@jandrabek.cz>
2010-08-21 15:49:46 +0200

    * Favicon + logo pro FB sharer * Zapomenuty helppresenter

--- leganto/app/WebModule/presenters/HelpPresenter.php
+++ leganto/app/WebModule/presenters/HelpPresenter.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * The source file is subject to the license located on web
  * "http://code.google.com/p/preader/".
  *
  * @copyright	Copyright (c) 2009 Jan Papoušek (jan.papousek@gmail.com),
- *				Jan Drábek (repli2dev@gmail.com)
+ * 				Jan Drábek (repli2dev@gmail.com)
  * @link		http://code.google.com/p/preader/
  * @license		http://code.google.com/p/preader/
  */
@@ -15,28 +16,28 @@
  * @version		$Id$
  */
 class Web_HelpPresenter extends Web_BasePresenter {
+
 	public function renderDefault() {
 		$this->setPageTitle(System::translate("Help"));
 		$this->setPageDescription(System::translate("Are you lost in our page, or you just miss last puzzle? This page can answer nearly all questions."));
 		$this->setPageKeywords(System::translate("help, support, faq, how to, actions, how it works, tags, books, engine"));
 		$this->getTemplate()->data = Leganto::supportCategory()->getSelector()->findAllSortedByWeight();
 		$content = array();
-		foreach($this->getTemplate()->data as $item) {
-			 $content[$item->id_support_category] = Leganto::supportText()->getSelector()->findAllByCategory($item->id_support_category);
+		foreach ($this->getTemplate()->data as $item) {
+			$content[$item->id_support_category] = Leganto::supportText()->getSelector()->findAllByCategory($item->id_support_category);
 		}
 		$this->getTemplate()->content = $content;
 	}
 
 	public function renderCategory($id) {
 		$this->getTemplate()->category = Leganto::supportCategory()->getSelector()->find($id);
-		if(count($this->getTemplate()->category) == 0) {
+		if (count($this->getTemplate()->category) == 0) {
 			$this->redirect("default");
 		}
 		$this->setPageTitle($this->getTemplate()->category->name);
 		$this->setPageDescription(System::translate("On this page you can find all pages which belong to choosed category. Select page with content which bother you."));
 		$this->setPageKeywords(System::translate("problems, help, support, answers, how it works"));
 		$this->getTemplate()->data = Leganto::supportText()->getSelector()->findAllByCategory($id);
-		
 	}
 
 	public function renderText($id) {
@@ -49,9 +50,10 @@ class Web_HelpPresenter extends Web_BasePresenter {
 	protected function createComponentSubmenu($name) {
 		$submenu = new SubmenuComponent($this, $name);
 		$data = Leganto::supportCategory()->getSelector()->findAllSortedByWeight();
-		foreach($data as $item) {
+		foreach ($data as $item) {
 			$submenu->addLink("category", $item->name, array("id" => $item->id_support_category));
 		}
 		return $submenu;
 	}
+
 }
