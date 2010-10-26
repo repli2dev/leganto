09d409be4a0b32b543509e8cbf7ae5ef7c55f20c
Jan Drabek <me@jandrabek.cz>
2010-10-26 17:11:48 +0200

    * Napoveda "Vedeli jste, ze..."

---
+++ resources/database/help.sql
@@ -0,0 +1,24 @@
+--- Vedeli jste, ze...
+--- (pro cestinu = 1)
+---
+
+INSERT INTO `help` (`id_help`, `id_language`, `category`, `text`, `image`) VALUES
+(1, 1, 'user', 'Můžete použít přihlášení přes Facebook nebo Twitter a zjednodušit si tak přihlášení. Pokud už máte účet, můžete jej se sociální sítí spojit dodatečně z Nastavení.', NULL),
+(2, 1, 'other', 'Můžete s ostatními čtenáři diskutovat v sekci Povídat si.', NULL),
+(3, 1, 'book', 'Můžete shlédnout TOP 200 nejlepších knih v sekci Nejlepší knihy.', NULL),
+(4, 1, 'other', 'Můžete najít další informace o tomto webu a o všemožných úkonech v sekci Nápověda.', NULL),
+(5, 1, 'other', 'Můžete vyhledávat odděleně knihy, autory, diskuze, uživatele. U vyhledávání knih a autorů můžete použít napovídání (stačí po napsání nejméně dvou znaků chvilku počkat).', NULL),
+(6, 1, 'user', 'Můžete u svého účtu vyplnit pohlaví a věk, aby ostatní věděli s kým mají tu čest. Na jejich základě můžete shlédnout působivé statistiky u knih a cíleněji se rozhodovat, kterou knihu si přečtete.', NULL),
+(7, 1, 'user', 'Můžete na stránce Aktuality  rozlišovat, zda je položka novým názorem nebo diskuzí - k tomu slouží ikonka knihy (pro názor) a tužky (pro diskuzní příspěvek) v pravé části položky.', NULL),
+(8, 1, 'book', 'Můžete vidět celkové hodnocení knihy nad obálkou a to v podobě procentuálního vyjádření, které je přesnější než počet hvězdiček.', NULL),
+(9, 1, 'book', 'Můžete knihy uspořádávat do poliček a v nich můžete nastavit přesné pořadí knih. Můžete tak dosáhnout poličky s nejoblíbenějšími knihami, vlastněnými knihami, či knihami, které si chcete přečíst.', NULL),
+(10, 1, 'user', 'Můžete sledovat jiné uživatele a vidět tak jednoduše jejich nově přidané názory a diskuzní příspěvky. Sledujte své známé, kamarády, uživatele s dobrým vkusem... Seznam uživatelů, které následujete vy, a kteří následují vás, najdete ve vašem profilu.', NULL),
+(11, 1, 'user', 'Můžete vidět, kteří uživatelé mají podobné názory na knihy jako vy. Jejich seznam si můžete zobrazit přes váš profil na podstránce Podobní uživatelé.', NULL),
+(12, 1, 'other', 'Můžete na stránce Aktuality vidět veškeré dění (aktivitu všech uživatelů) na našem serveru. Stačí v horní stránce kliknout na odkaz vše.', NULL),
+(13, 1, 'user', 'Můžete vaše příspěvky a názory opatřit vaším avatarem - obličejem, který umožní ostatním čtenářům velice rychle rozpoznat vaše příspěvky.', NULL),
+(14, 1, 'other', 'Můžete komentovat názory na knihy. Stačí kliknout na odkaz Odpovědět v pravém dolním rohu názoru a do nově zobrazeného formuláře zadat text vašeho komentáře, či dotazu.', NULL),
+(15, 1, 'book', 'Můžete přidat nové klíčové slovo u knihy bez přidání názoru. Na stránce knihy najdete pod klíčovými slovy formulář pro jeho vložení.', NULL),
+(16, 1, 'book', 'Můžete řadit názory na stránce knihy (na podstránce názory) podle času nebo hodnocení.', NULL),
+(17, 1, 'book', 'Můžete zobrazit podobné knihy nějaké vybrané knihy. Najdete je seřazené od nejpodobnější na stránce knihy na podstránce podobné knihy.', NULL),
+(18, 1, 'other', 'Pod tímto blokem se zobrazují poslední přidané názory? ', NULL),
+(19, 1, 'user', 'Můžete na svou osobní stránku nebo blog umístit ikonku čtenáře (obrázek) s třemi knihami, které jste přidali do svého deníku jako poslední. Více informací najdete v nápovědě.', NULL);
