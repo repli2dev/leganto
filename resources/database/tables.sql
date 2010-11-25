7c99c17cecd420f23b08a416baf9fcbe69e51953
Jan Drabek <me@jandrabek.cz>
2010-11-25 18:11:57 +0100

    * Oprava struktury DB

--- resources/database/tables.sql
+++ resources/database/tables.sql
@@ -314,8 +314,8 @@ CREATE TABLE `message` (
 	`id_user_to` INT UNSIGNED NOT NULL,
 	`content` TEXT NOT NULL,
 	`read` TINYINT(1) NULL DEFAULT  '0',
-	`deleted_by_owner` TINYINT( 1 ) NOT NULL DEFAULT  '0',
-	`deleted_by_recipient` TINYINT( 1 ) NOT NULL DEFAULT  '0',
+	`deleted_by_owner` TINYINT( 1 ) NULL DEFAULT  '0',
+	`deleted_by_recipient` TINYINT( 1 ) NULL DEFAULT  '0',
 	`inserted` DATETIME NOT NULL COMMENT 'cas, kdy byla polozka vlozena do systemu',
 	FOREIGN KEY (`id_user_from`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (`id_user_to`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE ON DELETE CASCADE
