9238bd4c10b4d507e851400b8b14f800ff780ae5
Jan Drabek <me@jandrabek.cz>
2011-03-23 14:37:03 +0100

    - Rozsireni feedu o shelving/deshelving eventy  - Rozrsireni import skriptu (deshelving nelze)

--- leganto/import/feed.php
+++ leganto/import/feed.php
@@ -26,17 +26,33 @@ $delimiter = "#$#";
 //	dibi::insert("feed_event",$content)->execute();
 //}
 // Change of opinion
-$result = dibi::query("SELECT * FROM view_opinion WHERE inserted != updated AND updated NOT LIKE '2010-11-20 17%'")->fetchAll();
+//$result = dibi::query("SELECT * FROM view_opinion WHERE inserted != updated AND updated NOT LIKE '2010-11-20 17%'")->fetchAll();
+//foreach($result as $row) {
+//	$content = array();
+//	$temp = array();
+//	$temp[] = $row["id_book_title"];
+//	$temp[] = $row["book_title"];
+//	$temp[] = $row["content"];
+//	$temp[] = null;
+//	$temp[] = $row["rating"];
+//	$temp[] = null;
+//	$content["type"] = FeedItemEntity::TYPE_UPDATED_OPINION;
+//	$content["id_user"] = $row["id_user"];
+//	$content["content"] = implode($delimiter, $temp);
+//	$content["inserted"] = $row["inserted"];
+//	dibi::insert("feed_event",$content)->execute();
+//}
+// Shelving
+$result = dibi::query("SELECT * FROM view_shelf_book WHERE 1")->fetchAll();
 foreach($result as $row) {
 	$content = array();
 	$temp = array();
+	$temp[] = $row["type"];
+	$temp[] = $row["name"];
 	$temp[] = $row["id_book_title"];
-	$temp[] = $row["book_title"];
-	$temp[] = $row["content"];
-	$temp[] = null;
-	$temp[] = $row["rating"];
-	$temp[] = null;
-	$content["type"] = FeedItemEntity::TYPE_UPDATED_OPINION;
+	$temp[] = $row["title"];
+	$temp[] = "";
+	$content["type"] = FeedItemEntity::TYPE_SHELVED;
 	$content["id_user"] = $row["id_user"];
 	$content["content"] = implode($delimiter, $temp);
 	$content["inserted"] = $row["inserted"];
